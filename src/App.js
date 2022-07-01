a74198cc40e1549870018e3a749eed151ff0f684
Enoch04 <Carlos.Ramirez.04@outlook.com>
2022-07-01 15:02:28 -0500

    Now in lesson 5

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-const App = () => {
-    const categories = [{
-            id: 1,
-            title: 'Hats',
-        },
-        {
-            id: 2,
-            title: 'Jackets',
-        },
-        {
-            id: 3,
-            title: 'Sneakers',
-        },
-        {
-            id: 4,
-            title: 'Womens',
-        },
-        {
-            id: 5,
-            title: 'Mens',
-        },
-    ];
+import Home from './routes/home/home.component';
 
+const App = () => {
     return (
-      <div className='categories-container'>
-        {categories.map(({ title }) => (
-          <div className='category-container'>
-            <div className='background-image' />
-            <div className='category-body-container'>
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    );
-  };
+      <Home />
+    )
+};
   
-  export default App;
+export default App;
