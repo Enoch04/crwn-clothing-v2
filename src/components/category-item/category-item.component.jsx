d09757f8b6c4e5416393115fc14683cd5ef2a18c
Enoch04 <Carlos.Ramirez.04@outlook.com>
2022-07-06 07:25:42 -0500

    deleted deprecated files

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,15 +1,9 @@
 import './category-item.styles.scss';
 
 const CategoryItem = ({ category }) => {
-<<<<<<< HEAD
   const { imageUrl, title} = category;
   return (
     <div key={id} className='category-container'>
-=======
-  const { imageUrl, title } = category;
-  return (
-    <div className='category-container'>
->>>>>>> origin/lesson-6
       <div
         className='background-image'
         style={{
