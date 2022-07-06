e81c8556b348d76e0ef87823051ac5df2300311a
Enoch04 <Carlos.Ramirez.04@outlook.com>
2022-07-06 10:12:16 -0500

    adding context to auth and creating a listener for auth user

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,13 +1,27 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from 'react';
+
+import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils';
 
 export const UserContext = createContext({
-  setCurrentUser: () => null,
   currentUser: null,
-});
+  setCurrentUser: () => null,
+})
 
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
-  const value = { currentUser, setCurrentUser };
+  const [currentUser,setCurrentUser] = useState(null);
+  const value ={ currentUser, setCurrentUser }; //Value is use to allow the children to change the state
+  
+ 
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-};
+  useEffect(() =>{
+    const unsubscribe = onAuthStateChangedListener(()=> {});
+    if(user){
+      createUserDocumentFromAuth(user);
+    }
+    setCurrentUser(user); 
+    return unsubscribe;
+  },[])
+  // All children that get wrap by UserProvider will get access to the value prop of 
+  // UserContext.provider 
+  return <UserContext.Provider value={value}>{ children }</UserContext.Provider>
+}
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -7,7 +7,9 @@ import {
   signInWithPopup, 
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword 
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
 } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
@@ -36,10 +38,8 @@ googleProvider.setCustomParameters({
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () =>
-  signInWithPopup(auth, googleProvider);
-export const signInWithGoogleRedirect = () =>
-  signInWithRedirect(auth, googleProvider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
@@ -85,3 +85,7 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth); 
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
